236b58abe20538d989e561c3bf12c3fb26af5aa0
CECBrijeshSebastian <chn25mca010@ceconline.edu>
2026-03-30 13:26:53 +0530

    update:File-manipulation-changed-to-one-single-program-named-Pgm26

--- java_day_8/Pgm26.java
+++ java_day_8/Pgm26.java
@@ -1,15 +1,36 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Pgm26 {
     public static void main(String[] args) {
         try {
-       
+
+	    Scanner sc = new Scanner(System.in);
+            System.out.println("Enter The String To Write to the File : ");
+	    String str = sc.nextLine();
             FileWriter fw = new FileWriter("test.txt");
-            fw.write("Hello, this is my first file.\n");
-            fw.write("Learning Java file handling.");
+            fw.write(str);
             fw.close();
 
             System.out.println("Data written to file.");
+
+
+	    FileInputStream fin = new FileInputStream("test.txt");
+            FileOutputStream fout = new FileOutputStream("destination.txt");
+
+            int data;
+
+            while ((data = fin.read()) != -1) {
+                fout.write(data);
+            }
+
+            fin.close();
+            fout.close();
+
+            System.out.println("File copied successfully.");
+
+
+
 
             FileReader fr = new FileReader("test.txt");
             BufferedReader br = new BufferedReader(fr);
@@ -28,3 +49,5 @@ public class Pgm26 {
         }
     }
 }
+
+
